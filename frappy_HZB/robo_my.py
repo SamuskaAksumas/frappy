d1ce3a5d8508cbfac7bbf83fbf96adcb53a99c35
SamuskaAksumas <m.korkot123@gmail.com>
2024-08-28 10:12:26 +0200

    change func line under init() to pass error but didnt work

--- frappy_HZB/robo_my.py
+++ frappy_HZB/robo_my.py
@@ -192,52 +192,6 @@ class Robot(HasIO,Drivable):
                            visibility = 'expert', #issue:closing raises KeyError: True when grabbing (closing gripper)
                            default = 0,
                            readonly = True)
-    
-    def euler_to_quaternion(xr, yr, zr, degree=False):
-        '''
-        Input:
-        Rotation-Angles: xr,yr,zr
-        degree: Bool-Type, is in degree
-
-        Converts Euler-Angles into Quaternion-Angles
-        '''
-
-        # create the rotation from euler angles
-        rotation = R.from_euler('xyz', [xr, yr, zr], degrees=degree)
-
-        # convert rotations into quaternions
-        quaternion = rotation.as_quat()
-
-        return quaternion
-    
-    def local_to_global(local_point, origin_point, degree=False):
-        '''
-        Input:
-        Local-Point: Point in Local-Coordinatesystem (in Shape X,Y,Z,XR,YR,ZR).
-        Simply type in 0 for Rs if there is no rotation
-        Origin-Point: Origin-Point of Local-Coordinatesystem, same Shape as Local-Point
-        degree: Bool-Type, is in degree
-
-        Converts Local_Point in Local-Coordinatesystem into Global-Point
-        '''
-
-        # create rotations from quaternions
-        local_rot = R.from_quat(euler_to_quaternion(local_point[3],local_point[4],local_point[5]))
-        origin_rot = R.from_quat(euler_to_quaternion(origin_point[3],origin_point[4], origin_point[5]))
-        
-        # combine rotations
-        combined_rotation = origin_rot * local_rot
-        
-        # apply combined rotations of local-point
-        rotated_point = combined_rotation.apply(local_point[:3])
-        
-        # tranlate point to global-coordinatesysten
-        global_point = rotated_point + origin_point[:3]
-        
-        # combine rotations for global-point
-        global_rotation = combined_rotation.as_euler('xyz', degrees=degree)
-        
-        return np.concatenate((global_point, global_rotation))
 
 
     def initModule(self):
@@ -343,6 +297,53 @@ class Robot(HasIO,Drivable):
                 return IDLE, 'Robot on without any prorgam running'
         else:
             return STOPPED, 'Robot not running at all'
+        
+
+    def euler_to_quaternion(xr, yr, zr, degree=False):
+        '''
+        Input:
+        Rotation-Angles: xr,yr,zr
+        degree: Bool-Type, is in degree
+
+        Converts Euler-Angles into Quaternion-Angles
+        '''
+
+        # create the rotation from euler angles
+        rotation = R.from_euler('xyz', [xr, yr, zr], degrees=degree)
+
+        # convert rotations into quaternions
+        quaternion = rotation.as_quat()
+
+        return quaternion
+    
+    def local_to_global(local_point, origin_point, degree=False):
+        '''
+        Input:
+        Local-Point: Point in Local-Coordinatesystem (in Shape X,Y,Z,XR,YR,ZR).
+        Simply type in 0 for Rs if there is no rotation
+        Origin-Point: Origin-Point of Local-Coordinatesystem, same Shape as Local-Point
+        degree: Bool-Type, is in degree
+
+        Converts Local_Point in Local-Coordinatesystem into Global-Point
+        '''
+
+        # create rotations from quaternions
+        local_rot = R.from_quat(euler_to_quaternion(local_point[3],local_point[4],local_point[5]))
+        origin_rot = R.from_quat(euler_to_quaternion(origin_point[3],origin_point[4], origin_point[5]))
+        
+        # combine rotations
+        combined_rotation = origin_rot * local_rot
+        
+        # apply combined rotations of local-point
+        rotated_point = combined_rotation.apply(local_point[:3])
+        
+        # tranlate point to global-coordinatesysten
+        global_point = rotated_point + origin_point[:3]
+        
+        # combine rotations for global-point
+        global_rotation = combined_rotation.as_euler('xyz', degrees=degree)
+        
+        return np.concatenate((global_point, global_rotation))
 
 
     @Command(group ='control')
